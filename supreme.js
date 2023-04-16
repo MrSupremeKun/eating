daef818ca1cb60035c75c7ef32700a17c11cf968
supreme <khojiakbarumarovf@gmail.com>
2023-04-16 09:51:46 +0300

    healthy eating

---
+++ supreme.js
@@ -0,0 +1,13 @@
+// let element = document.getElementById('.slider');
+// window.mySwipe = new Swipe(element, {
+//   startSlide: 0,
+//   auto: 3000,
+//   draggable: false,
+//   autoRestart: false,
+//   continuous: true,
+//   disableScroll: true,
+//   stopPropagation: true,
+//   callback: function(index, element) {},
+//   transitionEnd: function(index, element) {}
+// });
+// window.mySwipe = $('#mySwipe').Swipe().data('Swipe');
